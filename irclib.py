e698f5d34f881231fa0d70b322915d4fb7236e5e
TechDudie <73961295+TechDudie@users.noreply.github.com>
2022-03-09 12:01:02 -0600

    Update irclib.py

--- irclib.py
+++ irclib.py
@@ -10,8 +10,8 @@ import string
 import sys
 HOST = "irc.libera.chat"
 PORT = 6667
-NICK = "Portal"
-PASSWORD = os.getenv("PASSWORD")
+NICK = "TechDude"
+#PASSWORD = os.getenv("PASSWORD")
 CHANNEL = "##techdudeserver"
 #CHANNEL = "##BlockySurvival"
 SERVER = ""
@@ -23,10 +23,7 @@ s = socket.socket()
 s.connect((HOST, PORT))
 send(bytes("NICK %s\r\n" % NICK, "UTF-8"))
 send(bytes("USER %s %s %s :%s\r\n" % (NICK, NICK, NICK, NICK), "UTF-8"))
-#s.send(bytes("PRIVMSG NickServ regain {} {}\r\n".format(NICK, PASSWORD), "UTF-8"))
-#s.send(bytes("PRIVMSG NickServ identify {} {}\r\n".format(NICK,PASSWORD), "UTF-8"))
 send(bytes("JOIN {}\r\n".format(CHANNEL), "UTF-8"))
-s.send(bytes("PRIVMSG NickServ :identify {}\r\n".format(PASSWORD), "UTF-8"))
 readbuffer = readbuffer + s.recv(1024).decode("UTF-8")
 temp = str.split(readbuffer, "\n")
 readbuffer = temp.pop()
@@ -46,46 +43,3 @@ while 1:
         tokens = msg.split()
         if msg == "$hello":
             send("PRIVMSG {} :Hello!\r\n".format(CHANNEL).encode("utf-8"))
-        if msg == "$ping":
-            send("PRIVMSG {} :Pong!\r\n".format(CHANNEL).encode("utf-8"))
-        if msg == "$random":
-            send("PRIVMSG {} :{}\r\n".format(CHANNEL, random.randint(
-                0, 100)).encode("utf-8"))
-        if msg.startswith("$youtube "):
-            html = requests.get(
-                "https://www.youtube.com/results?search_query=" +
-                " ".join(msg.split()[1:])).content
-            video_ids = re.findall(r"watch\?v=(\S{11})", html.decode())
-            send("PRIVMSG {} :https://www.youtube.com/watch?v={}\r\n".format(
-                CHANNEL, video_ids[0]).encode("utf-8"))
-        if msg.startswith("$google "):
-            send("PRIVMSG {} :{}\r\n".format(
-                CHANNEL,
-                googlesearch.search(" ".join(msg.split()[1:]))[0]).encode("utf-8"))
-        if msg.startswith("$wolfram "):
-            send("PRIVMSG {} :{}\r\n".format(
-                CHANNEL,
-                wolfram.get(" ".join(msg.split()[1:]))).encode("utf-8"))
-        if msg.startswith("$github "):
-            if tokens[1] == "url":
-                send("PRIVMSG {} :https://github.com/{}/{}\r\n".format(
-                    CHANNEL, tokens[2], tokens[3]).encode("utf-8"))
-            if tokens[1] == "issues":
-                send("PRIVMSG {} :#{}: {}\r\n".format(
-                    CHANNEL, tokens[4],
-                    github.get_issue_title(tokens[2], tokens[3],
-                                           tokens[4])).encode("utf-8"))
-        if msg == "$help":
-            send("PRIVMSG {} :Avalible commands: $hello, $ping, $youtube, $google, $github, $wolfram.\r\n".format(CHANNEL).encode("utf-8"))
-        if msg.startswith("$help "):
-            if tokens[1] == "hello":
-                send("PRIVMSG {} :Syntax: $hello  Action: Says \"Hello!\".\r\n".format(CHANNEL).encode("utf-8"))
-            if tokens[1] == "ping":send("PRIVMSG {} :Syntax: $ping  Action: Says \"Ping!\".\r\n".format(CHANNEL).encode("utf-8"))
-            if tokens[1] == "youtube":
-                send("PRIVMSG {} :Syntax: $youtube <keyword> Action: Sends the URL of a YouTube video matching the keyword given.\r\n".format(CHANNEL).encode("utf-8"))
-            if tokens[1] == "google":
-                send("PRIVMSG {} :Syntax: $google <keyword> Action: Sends the URL of a google search with the keyword given\r\n".format(CHANNEL).encode("utf-8"))
-            if tokens[1] == "github":
-                send("PRIVMSG {} :Syntax: $github <topic> <user> <repo> <number> Action: Returns data about a github repo.\r\n".format(CHANNEL).encode("utf-8"))
-            if tokens[1] == "wolfram":
-                send("PRIVMSG {} :Syntax: $wolfram <query> Action: Asks Wolfram|Alpha the query given.\r\n".format(CHANNEL).encode("utf-8"))
